e7d81e4fe47d1aa5417a520263068097da565276
Eduardo Esparza Quezada <a352877@uach.mx>
2023-10-05 21:37:34 -0600

    Subir los archivos

--- src/vistas/VistaGastosIngresos.java
+++ src/vistas/VistaGastosIngresos.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vistas;
 
 import controladores.CrearGastosIngresos;
@@ -10,15 +6,10 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author TecniDEV
- */
+
 public class VistaGastosIngresos extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form CrearProductoInternalFrame
-     */
+
     public VistaGastosIngresos() {
         initComponents();
     }
